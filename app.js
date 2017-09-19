aebd28853358ad10aedf052b2821ba76273dd4d9
JF Padilla <emailreel@gmail.com>
2017-09-19 16:34:16 -0400

    add UI renderer

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 function onReady() {
   var toDos = [];
-  var addToDoForm = document.getElementById('addToDoForm');
+  var addToDoButton = document.getElementById('addToDoButton');
 
   function createNewToDo() {
     var newToDoText = document.getElementById('newToDoText');
@@ -9,12 +9,33 @@ function onReady() {
       complete: false
     });
     newToDoText.value = '';
+
+    renderTheUI(toDos);
   }
 
-  addToDoForm.addEventListener('submit', (event) => {
+  function renderTheUI(toDos) {
+    var toDoList = document.getElementById('toDoList');
+
+    toDoList.innerHTML = '';
+
+    toDos.forEach(function(toDo) {
+      var newLi = document.createElement('li');
+      var checkbox = document.createElement('input');
+      checkbox.type = "checkbox";
+
+      newLi.innerHTML = toDo.title;
+      newLi.appendChild(checkbox);
+
+      toDoList.appendChild(newLi);
+    });
+  }
+
+  addToDoButton.addEventListener('click', (event) => {
     event.preventDefault();
     createNewToDo();
   });
+
+  renderTheUI(toDos);
 }
 
 window.onload = function() {
