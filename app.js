afefd5fecbfc5742eabf1518bb91717992deeba9
JF Padilla <emailreel@gmail.com>
2017-09-19 23:54:28 -0400

    remove debug output and slightly improve code indentation

--- app.js
+++ app.js
@@ -3,10 +3,7 @@ function onReady() {
   const addToDoButton = document.getElementById('addToDoButton');
   const clearCheckedForm = document.getElementById('clearCheckedForm');
 
-
-
   function renderTheUI(toDos) {
-    console.log(toDos);
     // auxiliary function
     function relableIndices(todoArr) {
       let i=0;
@@ -28,7 +25,7 @@ function onReady() {
       checkbox.checked = toDo.complete;
 
       let deleteButton = document.createElement('button');
-      deleteButton.textContent = "delete " + toDo.index;
+      deleteButton.textContent = "delete";
 
       newLi.innerHTML = toDo.title;
       newLi.appendChild(checkbox);
@@ -41,7 +38,6 @@ function onReady() {
           toDo.complete = false;
         else
           toDo.complete = true;
-        console.log("render after box checked");
         renderTheUI(toDos);
       });
 
@@ -49,7 +45,6 @@ function onReady() {
         event.preventDefault();
         toDos.splice(toDo.index,1);
         relableIndices(toDos);
-        console.log("render after task removed by delete button");
         renderTheUI(toDos);
       });
     });
@@ -61,42 +56,40 @@ function onReady() {
             checkExists=true;
         i++;
     }
-    console.log("checkExists is " + checkExists);
     if(checkExists) { // add button to clear checked tasks
-        let clearCheckedButton = document.createElement('button');
-        clearCheckedButton.textContent = 'clear checked';
-        clearCheckedForm.appendChild(clearCheckedButton);
+      let clearCheckedButton = document.createElement('button');
+      clearCheckedButton.textContent = 'clear checked';
+      clearCheckedForm.appendChild(clearCheckedButton);
 
-        clearCheckedButton.addEventListener('click', (event) => {
-          event.preventDefault();
-          // determine which tasks are checked
-          let checkedTasks = [];
+      clearCheckedButton.addEventListener('click', (event) => {
+        event.preventDefault();
+        // determine which tasks are checked
+        let checkedTasks = [];
 
-          toDos.forEach(function(toDo) {
-            if(toDo.complete==true) {
-              checkedTasks.push(toDo.index);
-            } // checkedTasks values are sorted
-          });
-
-          // remove checked tasks
-          let removed, j=0, nremoved=0;
-          checkedTasks.forEach(function(removeIndex) {
-            removed=false;
-            while(!removed) {
-              if(toDos[j].index==removeIndex) {
-                toDos.splice(removeIndex-nremoved,1);
-                removed=true;
-                nremoved++;
-              }
-              else
-                j++;
-            }
-          });
-
-          relableIndices(toDos);
-          console.log("render after clear checked");
-          renderTheUI(toDos);
+        toDos.forEach(function(toDo) {
+          if(toDo.complete==true) {
+            checkedTasks.push(toDo.index);
+          } // checkedTasks values are sorted
         });
+
+        // remove checked tasks
+        let removed, j=0, nremoved=0;
+        checkedTasks.forEach(function(removeIndex) {
+          removed=false;
+          while(!removed) {
+            if(toDos[j].index==removeIndex) {
+              toDos.splice(removeIndex-nremoved,1);
+              removed=true;
+              nremoved++;
+            }
+            else
+              j++;
+          }
+        });
+
+        relableIndices(toDos);
+        renderTheUI(toDos);
+      });
     }
   }
 
@@ -109,7 +102,6 @@ function onReady() {
     });
     newToDoText.value = '';
 
-    console.log("render after new to do added");
     renderTheUI(toDos);
   }
 
